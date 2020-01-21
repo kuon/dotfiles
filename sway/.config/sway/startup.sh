b989a8ff838dd4ac0f014ff8b92f261634ec806e
Nicolas goy <kuon@goyman.com>
2020-01-21 02:13:54 +0100

    Move to a simple startup script

---
+++ sway/.config/sway/startup.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+# Clearnup tmux
+pkill tmux
+
+
+# Backgroup
+swaybg -c "#06101a" &
+
+
+# Notification daemon
+mako &
+
+# Audio handling
+jack_control start
+cadence-pulse2jack
+pactl set-default-sink jack_out
+
+
+fcitx-autostart &
+ckb-next -b &
+syncthing-gtk &
+keepassxc &
+
